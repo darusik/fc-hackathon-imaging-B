b5cefa11d4e0dbe0cf6bad7f95300d9af89c7d1f
tb53gahe <timm.birka@stud.tu-darmstadt.de>
2022-06-23 18:11:24 +0200

    implemented evaluation

--- federated_training.py
+++ federated_training.py
@@ -4,23 +4,32 @@ import torch
 
 def evaluate(y_true, y_score):
     # Compute Accuracy
-    res = (y_score == y_true).to(torch.float64)
-    res = torch.sum(res, dim=0)
-    acc = res / y_true.shape[0]
+    eq = (y_score == y_true).to(torch.float64)
+    sum = torch.sum(eq, dim=0)
+    acc = sum / y_true.shape[0]
     
     # Compute auc score
-    auc = torch.tensor(roc_auc_score(y_true, y_score))
+    # auc = torch.tensor(roc_auc_score(y_true.detach().numpy(), y_score.detach().numpy()), average="samples")
+    auc = 0
+    for i in range(y_score.shape[1]):
+        label_auc = roc_auc_score(y_true[:, i], y_score[:, i])
+        auc += label_auc
+    auc /= y_true.shape[1]
 
     # Compute Precision & Recall
-    precision, recall, _, _ = precision_recall_fscore_support(y_true.numpy(), y_score.numpy(), average="samples")
+    precision, recall, _, _ = precision_recall_fscore_support(y_true.detach().numpy(), y_score.detach().numpy(), average="samples")
 
-    return acc, auc, torch.tensor(precision), torch.tensor(recall)
+    return acc, torch.tensor(auc), torch.tensor(precision), torch.tensor(recall)
 
 
 def train(model, train_loader, optimizer, criterion, device, evaluator=None):
     model.train()
 
     running_loss = 0.0
+    
+    y_true = torch.tensor([])
+    y_score = torch.tensor([]) 
+    
     counter = 0
 
     for i, (images, labels) in enumerate(tqdm(train_loader), 0):
@@ -31,23 +40,40 @@ def train(model, train_loader, optimizer, criterion, device, evaluator=None):
         optimizer.zero_grad()
     
         outputs = model(images)
-        loss = criterion(outputs, labels)
 
+        # Compute loss
+        loss = criterion(outputs, labels)
         running_loss += loss.item()
 
+        # Threshold for classes
+        tmp = (outputs > 0.7).to(labels.dtype)
+        y_true = torch.cat((y_true, labels), 0)
+        y_score = torch.cat((y_score, tmp), 0)
+
+        
         loss.backward()
         optimizer.step()
 
+    # Collect Metrics
+    acc, auc, precision, recall = evaluate(y_true, y_score)
+
+    # Average Loss
     epoch_loss = running_loss / counter
-    return epoch_loss
+    
+    return epoch_loss, acc, auc, precision, recall
 
 
 def test(model, test_loader, criterion, device, evaluator=None):
     model.eval()
 
+    # Init Loss
     running_loss = 0.0
+    
+    y_true = torch.tensor([])
+    y_score = torch.tensor([])
+    
     counter = 0
-
+    
     with torch.no_grad():
         for i, (images, labels) in enumerate(tqdm(test_loader), 0):
             counter += 1
@@ -55,8 +81,25 @@ def test(model, test_loader, criterion, device, evaluator=None):
             images, labels = images.to(device), labels.to(device)
             
             outputs = model(images)
+            
+            # Compute loss
             loss = criterion(outputs, labels)
             running_loss += loss.item()
 
+            # Threshold for classes
+            tmp = (outputs > 0.7).to(labels.dtype)
+            y_true = torch.cat((y_true, labels), 0)
+            y_score = torch.cat((y_score, tmp), 0)
+           
+    
+    # Collect Metrics
+    acc, auc, precision, recall = evaluate(y_true, y_score)
+    print(acc)
+    print(auc)
+    print(precision)
+    print(recall)
+
+    # Average Loss
     epoch_loss = running_loss / counter
-    return epoch_loss
+
+    return epoch_loss, acc, auc, precision, recall
